8b15a50e80b725f23e37e242046f21f4e3d2aac9
PRUDVINATH16 <patnamprudvinath2006@gmail.com>
2025-06-26 07:59:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,7 +72,9 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 /* async function graphAgent(topic) {
   let post = await writerNode(topic);
